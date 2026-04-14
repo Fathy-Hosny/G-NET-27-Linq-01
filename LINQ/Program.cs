cdd1041ee9f562fd699981ecaed369f96d8df8d1
Fathy Hosny <ali01120939266@gmail.com>
2026-04-14 21:52:11 +0200

    Q1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,23 @@
 ﻿
+using LINQ.DataSources;
+
 namespace LINQ
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			#region Q1
+
+			var seafoodProducts = Source.ProductList.Where(p => p.Category == "Seafood");
+
+			foreach (var product in seafoodProducts)
+			{
+				Console.WriteLine($"Name: {product.ProductName}, Price: {product.UnitPrice:c}");
+			}
+
+			#endregion
+
 
 		}
 	}
